b6f95c697d171e5be91b4e499b08190a7c7751c8
Houssamelbadaoui <helbad@aulaestudio.es>
2026-03-26 16:34:45 +0100

    Add button to add a task and display tasks

--- script.js
+++ script.js
@@ -13,3 +13,33 @@ function addTask(title) {
   const task = new Task(title);
   tasks.push(task);
 }
+
+// Add event listener
+const input = document.getElementById("taskInput");
+const addBtn = document.getElementById("addTaskBtn");
+
+addBtn.addEventListener("click", () => {
+  const title = input.value;
+
+  if (title === "") return;
+
+  addTask(title);
+  displayTasks();
+
+  input.value = ""; // clear the input
+});
+
+// function to display tasks
+function displayTasks() {
+  const container = document.getElementById("taskList");
+
+  container.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const div = document.createElement("div");
+    div.classList.add("task");
+
+    div.textContent = task.title;
+    container.appendChild(div);
+  });
+}
